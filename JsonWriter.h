0a2213802e2135a0ff2e68061d767a48f4888246
Marek Stancik <43235061+MarekStancik@users.noreply.github.com>
2020-01-09 14:04:14 +0100

    Fixed bugs,added escaping,no lvalue for now

--- JsonWriter.h
+++ JsonWriter.h
@@ -6,6 +6,13 @@
 
 namespace Json
 {
+	class Node;
+	template<typename T>
+	class Value;
+	class Object;
+	template<typename T>
+	class Array;
+
 	class Node {
 	public:
 		virtual std::ostream& write(std::ostream& os) const noexcept = 0;
@@ -15,15 +22,57 @@ namespace Json
 		}
 
 		virtual ~Node() {};
+
+		template<typename T>
+		static std::shared_ptr<Node> create(const T& rval);
+
+		static std::shared_ptr<Node> create(const Object& rval);
+
+		static std::shared_ptr<Node> create(const char *rval);
+
+		template<typename T>
+		static std::shared_ptr<Node> create(const std::initializer_list<T>& value);
+
+		template<typename T>
+		static std::shared_ptr<Node> create(const std::vector<T>& value);
 	};
+
+
+	//------------WriterImpl---------------//
+	template<typename T>
+	inline std::ostream& writeImpl(std::ostream& os, const T& value) {
+		return os << value;
+	}
+
+	inline std::ostream& writeImpl(std::ostream& os, const std::tm& value) {
+		return os << '\"' << std::put_time(&value, "%Y-%m-%dT%H:%M:%S") << '\"';
+	}
+
+	std::ostream& writeImpl(std::ostream& os, const std::string& value) {
+		std::ostringstream escaped;
+		for (char ch : value) {
+			switch (ch) {
+			case '\"':
+			case '\\':
+			case '/':
+				escaped << '\\' << ch;
+				break;
+			default:
+				escaped << ch;
+			}
+		}
+
+		return os << '\"' << escaped.str() << '\"';
+	}
+
 
 	template<typename T>
 	class Array : public Node {
 	private:
 		std::vector<T> children;
 	public:
-		Array(std::vector<T>&& children)
-			:children(std::move(children)) {}
+		Array(const std::vector<T>& children)
+			:children(children) {}
 
 		Array& operator()(T&& val) {
 			children.push_back(std::move(val));
@@ -33,10 +82,7 @@ namespace Json
 		virtual std::ostream& write(std::ostream& os) const noexcept override {
 			os << '[';
 			for (auto it = children.begin(); it != children.end(); ++it) {
-				if (std::is_same<T, std::string>::value || std::is_same<T, const char*>::value)
-					os << '\"' << *it << '\"';
-				else
-					os << *it;
+				writeImpl(os, *it);
 				if (std::next(it) != children.end())
 					os << ',';
 			}
@@ -45,18 +91,6 @@ namespace Json
 			return os;
 		}
 	};
-
-	template<typename T,
-		typename std::enable_if<!std::is_arithmetic<T>::value>::type * = nullptr>
-	std::ostream& writeImpl(std::ostream& os, const T& value) {
-		return os << '\"' << value << '\"';
-	}
-
-	template<typename T,
-		typename std::enable_if<std::is_arithmetic<T>::value>::type * = nullptr>
-	std::ostream& writeImpl(std::ostream& os, const T& value) {
-		return os << value;
-	}
 
 	template<typename T>
 	class Value : public Node {
@@ -67,14 +101,9 @@ namespace Json
 			:value(value) {}
 
 		virtual std::ostream& write(std::ostream& os) const noexcept override {
-			return writeImpl<T>(os, value);
+			return writeImpl(os, value);
 		}
 	};
-
-	template<>
-	std::ostream& Value<std::tm>::write(std::ostream& os) const noexcept {
-		return os << '\"' << std::put_time(&value, "%Y-%m-%dT%H:%M:%S") << '\"';
-	}
 
 	class Object : public Node {
 	private:
@@ -96,39 +125,50 @@ namespace Json
 		}
 
 
-		template<typename T,
-			typename std::enable_if<!std::is_same<typename std::remove_reference<T>::type, Object>::value>::type * = nullptr>
-		Object& operator()(const std::string& name,const T& value) {
-			children[name] = std::make_shared<Value<typename std::remove_reference<T>::type>>(value);
-			return *this;
-		}
-
-		template<typename T,
-			typename std::enable_if<std::is_same<typename std::remove_reference<T>::type, Object>::value>::type * = nullptr>
-		Object& operator()(const std::string& name, T&& value) {
-			children[name] = std::make_shared<Object>(std::forward<T>(value));
+		template<typename T>
+		Object& operator()(const std::string& name, const T& value) {
+			children[name] = Node::create(value);
 			return *this;
 		}
 
 		template<typename T>
-		Object& operator()(const std::string& name, std::initializer_list<T>&& value) {
-			children[name] = std::make_shared<Array<T>>(std::forward<std::initializer_list<T>>(value));
-			return *this;
-		}
-
-		template<typename T>
-		Object& operator()(const std::string& name, std::vector<T>&& value) {
-			children[name] = std::make_shared<Array<T>>(std::forward<std::vector<T>>(value));
+		Object& operator()(const std::string& name, const std::initializer_list<T>& value) {
+			children[name] = Node::create(value);
 			return *this;
 		}
 	};
+
+	template<typename T>
+	inline std::shared_ptr<Node> Node::create(const T& rval) {
+		return std::make_shared<Value<T>>(rval);
+	};
+
+	inline std::shared_ptr<Node> Node::create(const Object& rval) {
+		return std::make_shared<Object>(rval);
+	};
+
+	inline std::shared_ptr<Node> Node::create(const char* rval) {
+		return std::make_shared<Value<std::string>>(rval);
+	}
+
+	template<typename T>
+	inline std::shared_ptr<Node> Node::create(const std::initializer_list<T>& value) {
+		return std::make_shared<Array<T>>(value);
+	}
+
+	template<typename T>
+	inline std::shared_ptr<Node> Node::create(const std::vector<T>& value) {
+		return std::make_shared<Array<T>>(value);
+	}
 }
 
 
 /*Example
 int main()
 {
-	Object root;
+
+	Json::Object root;
+	root("ahoj", "ca\"wes");
 	root("stringProp", "marek")
 		("intProp",258)
 		("stringArray", { 
@@ -143,23 +183,38 @@ int main()
 			3,
 			4
 			})
-		("obj", Object()
+		("obj", Json::Object()
 			("objIntProp", 2)
 			("objDoubleProp", 20.5)
 			("arrInObj",{20,10})
 		)
-		("objArray", { Object()
+		("objArray", { Json::Object()
 			("objStrProp", "sevas")
 			("objDoubleProp", 20.5)
 			("arrInObjInArr",{20,10}),
-			Object()
+			Json::Object()
 			("objStrProp", "sevas2")
 			("objDoubleProp", 22.5)
-			("arrInObjInArr",{22,12})
+			("arrInObjInArr",{22,12}) 
 			});
-	std::cout << root << std::endl << Array<Object>({ Object()
+
+	std::vector<int> values = {1,2,3,4,5,6,7};
+	std::vector<Json::Object> jsonVect;
+	for (int val : values)
+	{
+		int refval = val + 5;
+		jsonVect.push_back(Json::Object()
+			("val", val)
+			("refVal", refval));
+	}
+	root("refObjArr", jsonVect);
+	std::time_t t = std::time(0);   // get time now
+	std::tm dt;
+	localtime_s(&dt,&t);
+	std::cout << root << std::endl << std::endl << Json::Array<Json::Object>({ Json::Object()
 			("objStrProp", "sevas")
 			("objDoubleProp", 20.5)
+			("date",dt)
 			("arrInObjInArr",{20,10}) });
 }*/
 
